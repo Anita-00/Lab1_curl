1c76490491122db71bde7a6f14085cb52411914e
Anita-00 <anita@DESKTOP-TPF9QF8.localdomain>
2022-01-10 23:39:20 -0700

    final question

--- Request_Version.py
+++ Request_Version.py
@@ -1,4 +1,5 @@
 import requests
 print(requests.__version__)
-r = requests.get('http://www.google.com/').text
-print(r) 
+#r = requests.get('http://google.com/').text
+r = requests.get('https://raw.githubusercontent.com/Anita-00/Lab1_curl/67228df53c32baed925fb99e81fe41f75237fde0/Request_Version.py?token=GHSAT0AAAAAABQOH7E6QMW45OY6FGB66QSUYO5EWUQ')
+print(r.text)
